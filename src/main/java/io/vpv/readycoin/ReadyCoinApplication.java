4d10a9be87643f318047aaf3524a15b1fe72897b
Venkateswara VP <venkateswara.vp@gmail.com>
2023-02-25 19:25:46 -0500

    Initial Commit

---
+++ src/main/java/io/vpv/readycoin/ReadyCoinApplication.java
@@ -0,0 +1,13 @@
+package io.vpv.readycoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadyCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadyCoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/vpv/readycoin/ReadyCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vpv.readycoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadyCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
